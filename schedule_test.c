f8034a9a216d881392fae1b040d68b7bd1cfe835
sadrakhamoshi <s.a.d.kh.121@gmail.com>
2021-07-12 16:55:36 +0000

    some changes

--- schedule_test.c
+++ schedule_test.c
@@ -4,14 +4,12 @@
 
 int main(int argc, char *argv[])
 {
-    int pid;
     int child_pid, wait_time, run_time;
     int wait_time2, run_time2;
-    int s1, s2;
-    pid = fork();
+    int pid = fork();
     if (pid < 0)
     {
-        printf(1, "fork faild!\n");
+        printf(1, "fork failed ...!\n");
         exit();
     }
     if (pid == 0)
@@ -19,38 +17,31 @@ int main(int argc, char *argv[])
         pid = fork();
         if (pid < 0)
         {
-            printf(1, "fork faild!\n");
+            printf(1, "fork failed ...!\n");
             exit();
         }
         if (pid == 0)
         {
-            for (int i = 0; i < 100000; i++)
-            {
-                i *= 5 + i / 20;
-            }
+            sleep(600);
         }
         else
         {
-            s1 = set_priority(90);
-            for (int i = 0; i < 100000; i++)
-            {
-                i *= 5 + i / 20;
-            }
+            int child_oldPriority = set_priority(10);
+            sleep(600);
+
             child_pid = waitx(&wait_time2, &run_time2);
-	    s1 = set_priority(20);
-            printf(1, "\n child wait time : %d , Parent RunTime : %d with cildpid : %d and s1 : %d\n", wait_time2, run_time2, child_pid, s1);
+            child_oldPriority = set_priority(20);
+            printf(1, "\n child wait time : %d , Parent RunTime : %d with cild pid : %d and child_oldPriority : %d\n", wait_time2, run_time2, child_pid, child_oldPriority);
         }
     }
     else
     {
-        s2 = set_priority(30);
-        for (int i = 0; i < 100000; i++)
-        {
-            i *= 5 + i / 20;
-        }
+        int parent_oldPriority = set_priority(80);
+        sleep(600);
+
         child_pid = waitx(&wait_time, &run_time);
-        s2 = set_priority(10);
-        printf(1, "\n Parent wait time : %d , Parent RunTime : %d with cildpid : %d and s2 : %d\n", wait_time, run_time, child_pid, s2);
+        parent_oldPriority = set_priority(40);
+        printf(1, "\n Parent wait time : %d , Parent RunTime : %d with cildpid : %d and parent_oldPriority : %d\n", wait_time, run_time, child_pid, parent_oldPriority);
     }
     exit();
 }
